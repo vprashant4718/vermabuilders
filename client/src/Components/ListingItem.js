db4dfc0b6828acd4df0d2faec9e4743c7bd66033
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-01 22:50:56 +0530

    Update ListingItem.js

--- client/src/Components/ListingItem.js
+++ client/src/Components/ListingItem.js
@@ -11,21 +11,21 @@ export default function ListingItem({ listing}) {
         
         <div className='pl-6 pr-6'>
         <div>
-            <h6 className='truncate w-60 font-semibold text-lg mb-1 my-4 mx-1 uppercase lg:w-44 lg:text-base'>{listing.name}</h6>
+            <h6 className='truncate w-44 font-semibold text-lg mb-1 my-4 mx-1 uppercase sm:w-44 lg:w-44 lg:text-base'>{listing.name}</h6>
         </div>
         <div className='flex flex-row mb-1 items-center gap-1'>
           <MdLocationOn  className='text-green-600 ' />
-          <p className='truncate w-60 text-sm font-semibold lg:w-44'>{listing.address}..</p>
+          <p className='truncate w-44 text-sm font-semibold lg:w-44'>{listing.address}..</p>
         </div>
         <div className='flex flex-col  gap-1 text-sm font-semibold'>
-          <p className='truncate w-60  lg:w-44 '>{listing.description}</p>
+          <p className='truncate w-44  lg:w-44 '>{listing.description}</p>
          
-          <p className='truncate w-60 text-slate-700 font-semibold text-lg '>
+          <p className='truncate w-44 text-slate-700 font-semibold text-lg '>
           { listing.type === 'rent'?'$': '₹'  } {listing.regularprice}{ listing.type === 'rent'?'/month': '/Lacs'  }
           </p>
          
         </div>
-            <div className='flex flex-row gap-2 w-60 text-sm font-semibold items-center mb-3'>
+            <div className='flex flex-row gap-2 w-44 text-sm font-semibold items-center mb-3'>
               <p>{listing.bedrooms} beds</p>
               <p>{listing.bathrooms} baths</p>
             </div>
