8979479ed9e0ad3dff63ac91888d534f8a0d2fbb
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-02 11:39:17 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,10 @@
+const flowbiteReact = require("flowbite-react/plugin/tailwindcss");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", ".flowbite-react\\class-list.json"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbiteReact],
 }
