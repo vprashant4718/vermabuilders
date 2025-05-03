8dd980e2b5ac29c6e2fcfa4be5a904445dceeb74
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-03 21:43:34 +0530

    Update auth.route.js
    added new routes

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { signup, signin, google, signOut } from '../controllers/auth.controller.js';
+import { signup, signin, google, signOut, email, validateOtp } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/email', email);
+router.post('/validateotp', validateOtp);
 router.post('/signin', signin);
 router.post('/google', google);
 router.get('/signout',signOut)
