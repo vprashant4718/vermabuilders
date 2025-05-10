4194aa407c8d8f431760a59102ecd055a856b417
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-10 15:56:23 +0530

    Update email.js

--- api/routes/email.js
+++ api/routes/email.js
@@ -1,23 +1,25 @@
 import nodemailer from 'nodemailer';
 import { emailTemplate } from './emailTemplate.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
+  host: process.env.GMAIL_HOST,
+  port: process.env.GMAIL_PORT,
   secure: true, // true for port 465, false for other ports
   auth: {
-    user: "vermabuilders07@gmail.com",
-    pass: "ncxm dotx pxkd zefm",
+    user: process.env.GMAIL,
+    pass: process.env.GMAIL_PASSWORD,
   },
 });
 
 // async..await is not allowed in global scope, must use a wrapper
-export const Verification =Math.floor(100000 + Math.random() * 900000);
+export const Verification =Math.floor(100000 + Math.random() * 900000).toString();
 export const OtpMail = async(email) =>{
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: '"Verma Builders👻" <vermabuilders07@gmail.com>', // sender address
+    from: `"Verma Builders👻" <${process.env.GMAIL}>`, // sender address
     to: email, // list of receivers
     subject: "Verification Otp", // Subject line
     text: "otp for ", // plain text body
