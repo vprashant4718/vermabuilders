61466c5b92b6baabce305f95ac20dde9bc33abb4
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-10 17:37:39 +0530

    Update user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,15 +1,17 @@
 
 import express from 'express';
-import { userUpdate, userDelete, userListing, getUser } from '../controllers/user.controller.js';
+import { userUpdate, userDelete, userListing, getUser, userPasswordUpdate } from '../controllers/user.controller.js';
 
 import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.post('/update/:id', verifyToken, userUpdate)
-router.delete('/delete/:id', verifyToken, userDelete) 
-router.get('/listing/:id', verifyToken, userListing) 
-router.get('/:id', verifyToken, getUser) 
+router.post('/update/:id', verifyToken, userUpdate);
+router.post('/resetpassword', userPasswordUpdate);
+router.delete('/delete/:id', verifyToken, userDelete);
+router.get('/listing/:id', verifyToken, userListing);
+router.get('/:id', verifyToken, getUser);
+
  
 
 
