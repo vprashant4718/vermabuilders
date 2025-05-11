ec07e943032ae9ace6a05cadb10f05ecb0256ece
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-11 19:14:25 +0530

    Update ContactVerma.js

--- client/src/pages/ContactVerma.js
+++ client/src/pages/ContactVerma.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import logo from '../Components/vermaBuilders.png'
 import { FiPhoneCall } from "react-icons/fi";
 import { IoLocation } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
@@ -8,26 +6,27 @@ import { TbClockHour9Filled } from "react-icons/tb";
 
 export default function ContactVerma() {
   return ( 
-      <div className="pt-40  flex flex-row gap-5 w-[32rem] flex-wrap justify-center items-center m-auto  mb-5"> 
+      <>      <h1 className='text-3xl text-center font-bold py-3 px-3 mb-3 pt-40'>Contact Us</h1>
+      <div className="  flex flex-row gap-5  flex-wrap justify-center items-center m-auto  mb-5 sm:w-[32rem]"> 
 
 
-      <div className="p-3 flex flex-col items-center justify-center m-auto  w-60 h-60 border-2 rounded hover:bg-black hover:text-white ease-in-out transition">
+      <div className="p-3 flex flex-col items-center justify-center m-auto h-60 border-2 rounded hover:bg-black hover:text-white ease-in-out transition">
             <FiPhoneCall  className='text-4xl'/>
             <h5 className="text-xl font-bold">Phone Number </h5>
             <p className=" font-semibold text-lg w-52 text-center" > +91 9828811112</p>
       </div>
-      <div className="p-3 flex flex-col items-center justify-center m-auto  w-60 h-60 border-2 rounded hover:bg-black hover:text-white ease-in-out transition">
+      <div className="p-3 flex flex-col items-center justify-center m-auto h-60 border-2 rounded hover:bg-black hover:text-white ease-in-out transition">
       <IoLocation className='text-4xl'/>
             <h5 className="text-xl font-bold">Location </h5>
-            <p className=" font-semibold text-lg w-52 text-center" > Near Rawal Hospital, Ajmer Road, pincode 302026, Jaipur Rajasthan. </p>
+            <p className=" font-semibold text-lg w-52 text-center" > Near Rawal Hospital, Samurai Palace, Bhankrota, Ajmer Road, Jaipur Rajasthan, pincode 302026. </p>
       </div>
-      <div className="p-3 flex flex-col items-center justify-center m-auto  w-60 h-60 border-2 rounded hover:bg-black hover:text-white ease-in-out transition">
+      <div className="p-3 flex flex-col items-center justify-center m-auto h-60 border-2 rounded hover:bg-black hover:text-white ease-in-out transition">
       <MdEmail  className='text-4xl'/>
             <h5 className="text-xl font-bold">Email </h5>
             <p className=" font-semibold text-lg w-52 text-wrap text-center" > vermabuilders07  @gmail.com
             </p>
       </div>
-      <div className="p-3 flex flex-col items-center justify-center m-auto  w-60 h-60 border-2 rounded hover:bg-black hover:text-white ease-in-out transition">
+      <div className="p-3 flex flex-col items-center justify-center m-auto h-60 border-2 rounded hover:bg-black hover:text-white ease-in-out transition">
       <TbClockHour9Filled className='text-4xl'/>
             <h5 className="text-xl font-bold">Working Hours </h5>
             <p className=" font-semibold text-lg w-52 text-center" > Daily </p>
@@ -36,6 +35,6 @@ export default function ContactVerma() {
       
 
       </div>
-
+</>
   )
 }
