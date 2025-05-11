a80c9be0bf225b6f25e58f2fe067ab85506ee8e2
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-11 17:09:17 +0530

    Update About.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,13 +4,13 @@ import owner from '../Components/owner.png'
 
 export default function About() {
   return ( 
-   < div className='pt-40 mb-3'>
-  <div className=" flex flex-row justify-center items-center mb-3 gap-32"> 
-    <img src={logo}   className='w-100 h-100 bg-white ' alt='logo'/>
-      <div className="p-3 flex flex-col w-50">
+< div className=' items-center m-auto pt-40 mb-3 w-auto p-3 md:w-[60%]'>
+  <div className=" flex flex-col justify-center items-center mb-3 gap-16 sm:flex-row lg:gap-32"> 
+    <img src={logo}   className='w-auto h-auto bg-white ' alt='logo'/>
+      <div className="p-3 flex flex-col w-auto">
             <h1 className="text-3xl font-bold">About Us </h1>
             
-            <p className="text-slate-600  font-semibold text-lg w-96" > 
+            <p className="text-slate-600  font-semibold text-lg w-auto" > 
             Our primary expertise lies in property sale, purchase, and resale services, along with delivering high-quality construction (builder) projects. Whether you're looking to buy, sell, invest, or build, Verma Builders provides complete, transparent, and reliable solutions to meet all your property needs.
             </p>
       </div>
@@ -18,24 +18,25 @@ export default function About() {
       </div>
 
 <br/>
-<hr className='w-[50rem]  m-auto'/>
-  <div className="pt-12 flex flex-row justify-center items-center w-50 gap-32  mb-5"> 
-      <div className="p-3 flex flex-col w-50">
+<hr className='w-auto  m-auto'/>
+  <div className="pt-12 flex flex-col justify-center items-center w-auto gap-16 sm:flex-row lg:gap-16"> 
+      <div className="p-3 flex flex-col w-auto">
             <h1 className="text-3xl font-bold">Owner</h1>
             
-            <p className="text-slate-600  font-semibold text-lg w-96" > 
+            <p className="text-slate-600  font-semibold text-lg w-auto" > 
             At Verma Builders, we don't just build structures — we build trust, dreams, and futures.
-            Founded by Chandra Prakash Verma, a visionary leader with a passion for excellence, we have earned a reputation for delivering superior quality in residential, commercial, and infrastructure projects.
+            Founded by <span className='text-bold text-xl'>Chandra Prakash Verma</span>, a visionary leader with a passion for excellence, we have earned a reputation for delivering superior quality in residential, commercial, and infrastructure projects.
               
             </p>
       </div> 
-    <img src={owner}   className='w-60 h-60 bg-white ' alt='owner'/>
+    <img src={owner}   className='w-auto h-60 bg-white ' alt='owner'/>
 
 
       </div> 
 
-<hr className='w-[50rem]  m-auto'/>
+<hr className='w-auto  m-auto'/>
 
   </div>
   )
 }
+
