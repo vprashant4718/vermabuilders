adb442b467c0047b321e67c813a4d0a114cc7b4f
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-13 13:56:54 +0530

    Update user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,28 +5,59 @@ import Listing from "../models/listing.model.js";
 
 
 //update user from profile page
-export const userUpdate =async (req,res, next)=>{
-    if(req.user.id !== req.params.id) return next(errorHandler(403, 'You can only change your own account'));
+export const userUpdate = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(403, 'You can only change your own account'));
+  }
 
-    try {
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  try {
+    // Check for duplicate username
+    if (req.body.username) {
+      const existingUser = await User.findOne({
+        username: req.body.username,
+        _id: { $ne: req.params.id }, // Exclude current user
+      });
+      if (existingUser) {
+        return next(errorHandler(400, 'Username already exists'));
+      }
     }
-    const updateUser = await User.findByIdAndUpdate(req.params.id,{
-        $set:{
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            avatar : req.body.avatar
-        }},{new: true});
-        
-        const {password, ...rest} = updateUser._doc;
-        
-        res.status(200).json(rest);
-    } catch (error) {
-        next(error)
+
+    // Check for duplicate email
+    if (req.body.email) {
+      const existingEmail = await User.findOne({
+        email: req.body.email,
+        _id: { $ne: req.params.id }, // Exclude current user
+      });
+      if (existingEmail) {
+        return next(errorHandler(400, 'Email already exists'));
+      }
     }
+
+    // Hash password if present
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
 
 //Update User Password
 export const userPasswordUpdate =async (req,res, next)=>{
