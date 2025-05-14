0b2727b9a9c36b9bd89d8797007a864d6e185e1b
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-14 17:25:51 +0530

    Update DashPosts.js

--- client/src/Components/DashPosts.js
+++ client/src/Components/DashPosts.js
@@ -1,8 +1,9 @@
-import { Button, Modal, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 'flowbite-react'
-import React, { useEffect, useState } from 'react'
+import { Button, Modal, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 'flowbite-react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineDelete } from 'react-icons/ai';
+import { MdLocationOn } from 'react-icons/md';
 import { PiWarningBold } from 'react-icons/pi';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 
 export default function DashPosts() {
   const {currentUser} = useSelector((state)=> state.user);
@@ -12,31 +13,36 @@ export default function DashPosts() {
   const [postIdtoDelete, setPostIdtoDelete] = useState(null);
   const [DeleteMessage, setDeleteMessage] = useState(null);
   const [errorMessage,setErrorMessage] = useState(null);
-  console.log(userPost);
+
+      const [listings, setListing] = useState([]);
+      const [Loading, setLoading] = useState(false);
+
   useEffect(() => {
-     const getpostdata=async()=>{
-      try {
-        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
-        const data = await res.json();
-        if(res.ok){
-          setUserPost(data.posts);
-          if(data.posts.length < 1){
-            setShowMore(false);
-          }
-        }
-        else{
+          const fetchListing=async()=>{
+      
+            setLoading(true);
+      
+            try {
+              
+              const res = await fetch(`/api/listing/getadminlisting`);
+              const data = await res.json();
+              if (data.success === false) { 
+                setLoading(false)
+            }
            
+            
+            setListing(data);
+            setLoading(false);
+            
+          } catch (error) {
+           console.log(error) 
+          }
+          }
+          if(currentUser.isAdmin){
+          fetchListing();
         }
+        }, []);
         
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    
-    if(currentUser.isAdmin){
-      getpostdata();
-    }
-  }, [currentUser._id])
   
 
   const handleShowMore = async()=>{
@@ -78,45 +84,89 @@ export default function DashPosts() {
       setshowmodal(false);
   }
   return (
-      <div className='w-48'>
-      <Table className='w-48'>
-        <TableHead>
-          <TableHeadCell>DATE UPDATE</TableHeadCell>
-          <TableHeadCell>POST IMAGE</TableHeadCell>
-          <TableHeadCell>POST TITLE</TableHeadCell>
-          <TableHeadCell>CATEGORY</TableHeadCell>
-          <TableHeadCell>DELETE</TableHeadCell>
-          <TableHeadCell>EDIT</TableHeadCell>
-        </TableHead>
-        {userPost.map((posts)=> <TableBody>
-        <TableRow>
-          <TableCell>{new Date(posts.createdAt).toLocaleDateString()}</TableCell>
-          <TableCell><img src={posts.image} alt="" width={30} height={20} /></TableCell>
-          <TableCell>{posts.title}</TableCell>
-          <TableCell>{posts.categories}</TableCell>
-          <TableCell><Button onClick={()=>{setshowmodal(true); setPostIdtoDelete(posts._id)}}>Delete</Button></TableCell>
-          <TableCell><Button>Update </Button></TableCell>
-          </TableRow>
-        </TableBody>)}
-      </Table>
+     
+<div className="relative pt-10 w-[80vw] overflow-x-auto h-[80vh] shadow-md sm:rounded-lg">
+    <div className="pb-4 bg-white pl-5">
+        <label for="table-search" className="sr-only">Search</label>
+        <div className="relative mt-1">
+            <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="text" id="table-search" className="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search for items" />
+        </div>
+    </div>
+    <table className="w-full p-5 text-sm text-left rtl:text-right text-gray-500 ">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+
+
+                <th scope="col" className="px-6 py-3">
+                   Date
+                </th>
+                <th scope="col" className="px-6 py-3">
+                   Image
+                </th>
+                <th scope="col" className="px-6 py-3">
+                   Title
+                </th>
+                <th scope="col" className="px-6 py-3">
+                   Location
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Category
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Update
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Delete
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {listings.map((listing)=><tr className="bg-white border-b text-gray-900   border-gray-200 hover:bg-gray-50 ">
+
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                   {new Date(listing.createdAt).toLocaleDateString()}
+                </th>
+                <td className="px-6 py-4 ">
+                   <img src={listing.imageUrl[0]} alt="" width={30} height={20} />
+                </td>
+                <td className="px-6 py-4">
+                    {listing.name}
+                </td>
+                <td className="px-6 py-4 flex flex-row gap-1 items-center">
+                 <MdLocationOn  className='text-green-600 ' />
+                    {listing.address}
+                </td>
+                <td className="px-6 py-4">
+                    {listing.type}
+                </td>
+                <td className="px-6 py-4 uppercase">
+                    {listing.type==='rent'? '₹ '+(listing.regularprice)+'K month': '₹ '+(listing.regularprice)+' Lacs'}
+                </td>
+                <td className="px-6 py-4">
+                    <button type='button' className='bg-blue-950 p-2 rounded text-white'>Update </button>
+                </td>
+                <td className="px-6 py-4">
+                   <button className='bg-red-700 p-2 rounded text-white' onClick={()=>{setshowmodal(true); setPostIdtoDelete(listing._id)}}>Delete</button>
+                </td>
+            </tr>)}
+            
+            </tbody>
+            </table>
+
 
       {
         showMore && 
-       (<button onClick={handleShowMore} className='mb-6 text-yellow-300 text-center border-solid border-yellow-300 m-auto'>+Show More</button>)
+       (<button onClick={handleShowMore} classNameName='mb-6 text-yellow-300 text-center border-solid border-yellow-300 m-auto'>+Show More</button>)
       }
-      <Modal show={showmodal} onClose={()=>{setshowmodal(false)}} popup size={'md'}  >
-              <Modal.Header />
-              <Modal.Body>
-                <div className="flex flex-col">
-                  <PiWarningBold className="w-14 h-14 m-auto dark:text-white"/>
-                  <div className="m-auto mb-3">Are you sure you want to delete Your Account</div>
-                </div>
-                <div className="flex flex-row m-auto justify-between">
-                <Button color={'red'} className="bg-red-600 text-white hover:text-black" onClick={()=>handleDeletePost()}>Yes i'm sure {<AiOutlineDelete className="text-xl text-black dark:text-white"/> }</Button>
-                <Button color={'red'} onClick={()=>{setshowmodal(false)}}>Cancel</Button>
-                </div>
-              </Modal.Body>
-            </Modal>
+     
 
       </div>
   )
