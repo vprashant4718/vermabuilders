1afe2c4c5656c50d1fa89114058da02d1c157c20
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-14 19:34:55 +0530

    Update Header.js

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -9,6 +9,7 @@ import logo from './vermaBuilders.png';
 export default function Header() {
   const {currentUser} = useSelector((state) => state.user);
   const [searchTerm, setsearchTerm] = useState('');
+  const [toggle, settoggle] = useState(false);
   const navigate = useNavigate();
   const handleSearchListing=(e)=>{
     e.preventDefault();
@@ -26,12 +27,26 @@ export default function Header() {
       setsearchTerm(getSearchTermFromQuery);
     } 
     
-  }, [window.location.search])
+  }, [window.location.search]);
+
+
+  function toggleMenu() {
+    const navbar = document.getElementById('navbar-default');
+    const toggleBtn = document.getElementById('toggle-btn');
+    if (navbar && toggleBtn) {
+      navbar.classList.toggle('hidden');
+      toggleBtn.classList.toggle('rotate-90');
+    }
+  }
+ 
+
+
   
   return (
     
-   <nav class="bg-gray-200 border-gray-200 fixed top-0 z-50 w-full shadow-md">
-   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    
+    <nav class="bg-gray-200 border-gray-200 fixed top-0 z-50 w-full shadow-md">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <Link to={'/'} class="flex items-center h-8 space-x-3 rtl:space-x-reverse">
       <img src={logo} className='w-16  md:w-30 md:h-20'  alt="verm builders logo" />
     </Link> 
@@ -46,7 +61,7 @@ export default function Header() {
           : (<Link to="/signin" className='items-center m-auto'><li className='underline mt-2 list-none self-center  sm:inline text-slate-700 hover:underline'>Sign In</li></Link>)
         }
         </div>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+    <button data-collapse-toggle="navbar-default" type="button" id='toggle-btn' class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg sm:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false" onClick={()=>{toggleMenu()}}>
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true"  fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -56,7 +71,7 @@ export default function Header() {
     <div class="hidden w-full sm:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col gap-2 p-4 md:p-0 mt-4 border bg-blue-900 border-gray-100 rounded-lg md:bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
         
-       <Link to="/">
+        <Link to="/">
              <li className='list-none sm:inline text-white md:text-slate-700 hover:underline block py-2 px-3 bg-blue-700 rounded-sm sm:bg-transparent  md:p-0' aria-current="page">Home</li>
         </Link>
         <Link to="/about">
@@ -68,7 +83,7 @@ export default function Header() {
 
         <div className='hidden md:inline '>
   {currentUser ?( <Link to="/profile"> <img src={currentUser.avatar} width={40} height={40} className='rounded-full' alt="profile" /></Link>)
-          : (<Link to="/signin" className='items-center m-auto'><li className='underline list-none self-center  sm:inline text-slate-700 hover:underline'>Sign In</li></Link>)
+          : (<Link to="/signin" className='items-center '><li className='underline  list-none self-center  sm:inline text-slate-700 hover:underline'>Sign In</li></Link>)
         }
         </div>
        
