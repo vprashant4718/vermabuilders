f65e3db81ec536cd593d244b4005a23309644f54
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-14 20:09:14 +0530

    Update Header.js

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -61,14 +61,14 @@ export default function Header() {
           : (<Link to="/signin" className='items-center m-auto'><li className='underline mt-2 list-none self-center  sm:inline text-slate-700 hover:underline'>Sign In</li></Link>)
         }
         </div>
-    <button data-collapse-toggle="navbar-default" type="button" id='toggle-btn' class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg sm:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false" onClick={()=>{toggleMenu()}}>
+    <button data-collapse-toggle="navbar-default" type="button" id='toggle-btn' class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false" onClick={()=>{toggleMenu()}}>
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true"  fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
 </div>
-    <div class="hidden w-full sm:block md:w-auto" id="navbar-default">
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col gap-2 p-4 md:p-0 mt-4 border bg-blue-900 border-gray-100 rounded-lg md:bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
         
         <Link to="/">
