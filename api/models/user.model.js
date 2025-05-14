b1d5eef70862ca0623d4156424994ff133aaf57d
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-14 22:35:07 +0530

    Update user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
     avatar:{
         type: String,
         default: "https://media.istockphoto.com/id/1316420668/vector/user-icon-human-person-symbol-social-profile-icon-avatar-login-sign-web-user-symbol.jpg?s=612x612&w=0&k=20&c=AhqW2ssX8EeI2IYFm6-ASQ7rfeBWfrFFV4E87SaFhJE="
+    },
+     isAdmin: {
+        type: Boolean,
+        default: false
     }
 }, {timestamps: true});
 
