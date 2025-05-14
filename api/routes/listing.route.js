e05391b357b06ef0ad7cd9a0de66881f633decd8
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-14 21:38:28 +0530

    Update listing.route.js

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken.js";
-import { createListing, deleteListing, getListing, UpdateListing, getSearchListings, getadminlisting} from "../controllers/listing.controller.js";
+import { createListing, deleteListing, getListing, UpdateListing, getSearchListings, getadminlisting, deletePostAdmin} from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.delete('/deletepostadmin/:id', verifyToken, deletePostAdmin);
 router.post('/update/:id', verifyToken, UpdateListing);
 router.get('/getlisting/:id', getListing);
 router.get('/get', getSearchListings);
 router.get('/getadminlisting', getadminlisting);
 
 export default router;
-
