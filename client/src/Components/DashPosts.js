4e532eda47d79ee4115eb943c9245272de9ff559
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-22 14:42:35 +0530

    Update DashPosts.js

--- client/src/Components/DashPosts.js
+++ client/src/Components/DashPosts.js
@@ -127,9 +127,7 @@ const [searchTerm, setSearchTerm] = useState("");
                 <th scope="col" className="px-6 py-3">
                     Price
                 </th>
-                <th scope="col" className="px-6 py-3">
-                    Update
-                </th>
+                 
                 <th scope="col" className="px-6 py-3">
                     Delete
                 </th>
@@ -158,9 +156,7 @@ const [searchTerm, setSearchTerm] = useState("");
                 <td className="px-6 py-4 uppercase">
                     {listing.type==='rent'? '₹ '+(listing.regularprice)+'K month': '₹ '+(listing.regularprice)+' Lacs'}
                 </td>
-                <td className="px-6 py-4">
-                    <button type='button' className='bg-blue-950 p-2 rounded text-white'>Update </button>
-                </td>
+                
                 <td className="px-6 py-4">
                    <button className='bg-red-700 p-2 rounded text-white' onClick={()=>{setshowmodal(true); setPostIdtoDelete(listing._id)}}>Delete</button>
                 </td>
