3c2a44417291c2749141c7dc4020b59df3e824b4
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-23 15:36:09 +0530

    Update Contact.js

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,20 +1,18 @@
  
 import { useEffect, useState } from 'react';
+import {toast } from 'react-toastify';
 
-
-export default function Contact({listing}) {
+export default function Contact({listing, currentUser}) { 
   const [owner, setowner] = useState(null);
-  const [formdata, setFormdata] = useState({});
+  const [formdata, setFormdata] = useState({username: currentUser.username, email:currentUser.email, phone: '', message: ''});
 
   useEffect(() => {
-
     try {
-      
       const fetchUser = async()=>{
         const res = await fetch(`/api/user/${listing.userRef}`);
         const data =  await res.json();
         if (data.success === false) {
-          return console.log(data.message); 
+          return toast.error(data.message); 
         }
         
         setowner(data);
@@ -26,11 +24,12 @@ export default function Contact({listing}) {
     }
     }, [listing.userRef]);
   
-  console.log(owner)
+
 
 const handleOnChange=(e)=>{
    setFormdata({...formdata, [e.target.name]: e.target.value}); 
 }
+
 
 
 const sendContactDetails = async()=>{
@@ -44,23 +43,21 @@ const sendContactDetails = async()=>{
     });
     const data = await res.json();
     if (data.success === false) {
-      return console.log(data.message);
+      return toast.error(data.message);
     }
-    console.log(data);
+    toast.success(data.message);
   } catch (error) {
-    console.log(error);
+    toast.error(error);
   }
 }
 
-
-console.log(formdata);
   return (
    
     <div className='flex flex-col gap-3'>
       <p>Contact <span className='italic font-semibold'> {owner && owner.username} </span>for <span className='italic font-semibold'>{ listing.name}</span> </p>
       <input type="text" name="phone" id="phone" placeholder='Enter Your Phone' onChange={handleOnChange} className='mr-auto w-96 h-10 p-2 border border-slate-300 focus:outline-none mb-3 rounded-lg ' required/>
       <textarea name="message" id="message" placeholder='Enter Your Message...' onChange={handleOnChange} className='mr-auto w-96 h-24 p-2 border border-slate-300 focus:outline-none mb-3 rounded-lg ' ></textarea>
-      <button type='button' onClick={sendContactDetails} className='uppercase bg-slate-700 rounded text-lg text-white text-center w-96 p-2'>Send Message</button> 
+      <button type='button' onClick={sendContactDetails} className='uppercase bg-slate-700 rounded text-lg text-white text-center w-96 p-2 hover:opacity-85'>Send Message</button> 
         </div> 
     
   )
