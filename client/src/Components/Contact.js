e187318172b8eed73779da3ee4a9ca859206a086
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-05-23 16:58:42 +0530

    Update Contact.js

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -59,7 +59,7 @@ const sendContactDetails = async()=>{
 }
 
   return (
-   
+   <>
     <div className='flex flex-col gap-3'  id="contact">
       <p>Contact <span className='italic font-semibold'> {owner && owner.username} </span>for <span className='italic font-semibold'>{ listing.name}</span> </p>
       <input type="text" name="phone" id="phone" placeholder='Enter Your Phone' onChange={handleOnChange} className='mr-auto w-96 h-10 p-2 border border-slate-300 focus:outline-none mb-3 rounded-lg ' required/>
@@ -70,6 +70,6 @@ const sendContactDetails = async()=>{
     <div className='hidden' id="contact2">
       <span  className='uppercase bg-green-700 rounded text-lg text-white text-center w-96 p-2 hover:opacity-90'>Your Message Sended</button> 
         </div> 
-    
+ </>
   )
 }
