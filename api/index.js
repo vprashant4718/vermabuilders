fc8974dbb5c5e4504e01ccf125230871cad98b0e
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2025-12-13 15:55:41 +0530

    Change CORS origin to allow all origins

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: ["https://vermabuilders.vercel.app", "http://vermabuilders.onrender.com", "http://localhost:5173"],
+    origin: true,
     credentials: true
 }));
 
