725dfc131c1137fb3a045148bad1af595bb3350b
Prashant Verma <vprashant4718@gmail.com>
2025-12-14 15:07:56 +0530

    completed the application

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -2,10 +2,14 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
+import { Loader2Icon } from 'lucide-react';
+
 export default function Contact({ listing }) {
   const currentUser = useSelector((state) => state.user.currentUser);
   const [owner, setOwner] = useState(null);
+  const [messageSended, setMessageSended] = useState(false);
   const backendUrl = process.env.REACT_APP_BASE_URL;
+  const [loading, setLoading] = useState(false);
 
   const [formdata, setFormData] = useState({
     username: currentUser?.username || '',
@@ -18,15 +22,16 @@ export default function Contact({ listing }) {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const res = await fetch(`${backendUrl}/api/user/${listing.userRef}`);
+        const res = await fetch(`${backendUrl}/api/user/${listing.userRef}`, {
+          method:'GET',
+          credentials:'include'
+        });
         const data = await res.json();
         if (data.success === false) {
           return toast.error(data.message);
         }
 
         setOwner(data);
-       console.log(data);
-       console.log(owner);
         setFormData((prev) => ({ ...prev, email2: data.email }));
        
       } catch (error) {
@@ -36,7 +41,6 @@ export default function Contact({ listing }) {
 
     fetchUser();
   }, [listing.userRef]);
-  console.log(formdata);
 
 const handleOnChange=(e)=>{
    setFormData({...formdata, [e.target.name]: e.target.value}); 
@@ -45,10 +49,11 @@ const handleOnChange=(e)=>{
 
 
 const sendContactDetails = async()=>{
-  const sendMessage = document.getElementById('sendMessage');
-  const messageSent = document.getElementById('messagesend');
+  setLoading(true);
   if (!formdata.phone || !formdata.message) {
-    return toast.error('Please fill in all fields');
+    setLoading(false);
+    toast.error('Please fill in all fields');
+    return; 
   }
   
  
@@ -63,14 +68,15 @@ const sendContactDetails = async()=>{
     });
     const data = await res.json();
     if (data.success === false) {
+      setLoading(false);
       return toast.error(data.message);
     }
-
+    setLoading(false)
+    setMessageSended(true);
     toast.success('Message sent successfully');
-    sendMessage.style.display = 'none';
-    messageSent.style.display = 'block';
 
   } catch (error) {
+    setLoading(false);
     toast.error(error);
   }
 }
@@ -78,17 +84,25 @@ const sendContactDetails = async()=>{
   return (
    <>
        
-       <div className='flex flex-col gap-3' id='sendMessage'>
-      <p>Contact <span className='italic font-semibold'> {owner && owner.username} </span>for <span className='italic font-semibold'>{ listing.name}</span> </p>
+   {messageSended === false && <div className='flex flex-col gap-3' id='sendMessage'>
+
+      <p>
+        Contact 
+        <span className='italic font-semibold'> {owner && owner.username} </span>
+        for 
+        <span className='italic font-semibold'>
+          { listing.name}
+        </span> 
+      </p>
       <input type="text" name="phone" id="phone" placeholder='Enter Your Phone' onChange={handleOnChange} className='mr-auto w-96 h-10 p-2 border border-slate-300 focus:outline-none mb-3 rounded-lg ' required/>
       <textarea name="message" id="message" placeholder='Enter Your Message...' onChange={handleOnChange} className='mr-auto w-96 h-24 p-2 border border-slate-300 focus:outline-none mb-3 rounded-lg ' ></textarea>
-      <button type='button' onClick={sendContactDetails} className='uppercase bg-slate-700 rounded text-lg text-white text-center w-96 p-2 hover:opacity-85'>Send Message</button> 
-        </div> 
+      <button type='button' onClick={sendContactDetails} className='uppercase bg-slate-700 rounded text-lg text-white text-center flex justify-center items-center w-96 p-2 hover:opacity-85'>{loading? <Loader2Icon size={24}  className='animate-spin'/> : 'Send Message'}</button> 
+    </div>} 
 
 
-        <div className='flex flex-col gap-3 hidden' id='messagesend'>
-      <span className='uppercase bg-red-700 rounded text-lg text-white text-center w-96 p-2 mt-3 hover:opacity-85'>Message Send Successfully</span> 
-        </div> 
+    {messageSended === true && <div className='flex flex-col gap-3 ' id='messagesend'>
+      <span className='bg-green-700 rounded text-sm text-white text-center w-96 p-2 mt-3 hover:opacity-85'>Message Send Successfully</span> 
+    </div> }
     </>
 
     
